6620a7205e3ae3ef7bb230ed03e632e1152b9df8
Nathan Osman <admin@quickmediasolutions.com>
2014-05-12 18:57:44 -0700

    Initial commit.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='rss2producer',
+    version='0.1',
+    description="Simplifies the process of creating an RSS 2.0 feed.",
+    author='Nathan Osman',
+    author_email='admin@quickmediasolutions.com',
+    url='https://github.com/nathan-osman/rss2producer',
+    license='MIT',
+    packages=['rss2producer']
+)
