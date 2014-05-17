3c8e8b9e8b2fbe770f32f4f7abc43a72122cdc7d
Nathan Osman <admin@quickmediasolutions.com>
2014-05-16 21:30:09 -0700

    Added test for Unicode.

--- tests.py
+++ tests.py
@@ -21,3 +21,10 @@ class TestRSS2Feed:
         eq_(root.find('./channel/item/title').text, 'Title')
         eq_(root.find('./channel/item/link').text, 'http://example.org')
         eq_(root.find('./channel/item/description').text, 'Description')
+        eq_(root.find('./channel/item/pubDate').text, 'Thu, 01 Jan 1970 00:00:00 -0000')
+
+    def test_utf8(self):
+        hello_world = u'\u3053\u3093\u306b\u3061\u306f\u4e16\u754c'
+        feed = RSS2Feed(hello_world, '', '')
+        root = ElementTree.fromstring(feed.get_xml().encode('utf-8'))
+        eq_(root.find('./channel/title').text, hello_world)
