bbbac3ace4d58a705b408a451ee2bb12169d7863
Nathan Osman <admin@quickmediasolutions.com>
2014-05-16 21:08:41 -0700

    Switched to ElementTree for tests.

--- tests.py
+++ tests.py
@@ -1,16 +1,23 @@
 from datetime import datetime
 from nose.tools import eq_
+from xml.etree import ElementTree
+
 from rss2producer import RSS2Feed
 
-def test_empty():
-    f = RSS2Feed('', '', '')
-    eq_(f.get_xml(), '<?xml version="1.0" ?><rss version="2.0"><channel><title>' +
-                     '</title><link></link><description></description></channel></rss>')
+class TestRSS2Feed:
+    """Tests the RSS2Feed class."""
 
-def test_append():
-    f = RSS2Feed('', '', '')
-    f.append_item('', '', '', datetime(1970, 01, 01))
-    eq_(f.get_xml(), '<?xml version="1.0" ?><rss version="2.0"><channel><title>' +
-                     '</title><link></link><description></description><item><title>' +
-                     '</title><link></link><description></description><pubDate>' +
-                     'Thu, 01 Jan 1970 00:00:00 -0000</pubDate></item></channel></rss>')
+    def test_feed(self):
+        feed = RSS2Feed('Title', 'http://example.org', 'Description')
+        root = ElementTree.fromstring(feed.get_xml())
+        eq_(root.find('./channel/title').text, 'Title')
+        eq_(root.find('./channel/link').text, 'http://example.org')
+        eq_(root.find('./channel/description').text, 'Description')
+
+    def test_append(self):
+        feed = RSS2Feed('', '', '')
+        feed.append_item('Title', 'http://example.org', 'Description', datetime(1970, 01, 01))
+        root = ElementTree.fromstring(feed.get_xml())
+        eq_(root.find('./channel/item/title').text, 'Title')
+        eq_(root.find('./channel/item/link').text, 'http://example.org')
+        eq_(root.find('./channel/item/description').text, 'Description')
