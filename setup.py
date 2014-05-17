43c82f14b490d7e2f169b8f8e27000348ca9675c
Nathan Osman <admin@quickmediasolutions.com>
2014-05-16 23:26:37 -0700

    Bumped version to 0.1.1.

--- setup.py
+++ setup.py
@@ -2,11 +2,17 @@ from distutils.core import setup
 
 setup(
     name='rss2producer',
-    version='0.1',
+    version='0.1.1',
     description="Simplifies the process of creating an RSS 2.0 feed.",
     author='Nathan Osman',
     author_email='admin@quickmediasolutions.com',
     url='https://github.com/nathan-osman/rss2producer',
     license='MIT',
-    packages=['rss2producer']
+    packages=['rss2producer'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Topic :: Internet :: WWW/HTTP'
+    ]
 )
